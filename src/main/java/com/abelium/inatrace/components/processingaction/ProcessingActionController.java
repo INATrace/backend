50d1d19a42c0f02e667b41bd1075b74e01021c9a
MusicDin <dm6067@student.uni-lj.si>
2021-09-23 11:17:03 +0200

    Accept language as enum in processing action request

--- src/main/java/com/abelium/inatrace/components/processingaction/ProcessingActionController.java
+++ src/main/java/com/abelium/inatrace/components/processingaction/ProcessingActionController.java
@@ -63,9 +63,9 @@ public class ProcessingActionController {
 	@ApiOperation("Get a single processing action with the provided ID.")
 	public ApiResponse<ApiProcessingAction> getProcessingAction(
 			@Valid @ApiParam(value = "ProcessingAction ID", required = true) @PathVariable("id") Long id,
-			@Valid @ApiParam(value = "language") @RequestParam(value = "language", defaultValue = "EN") String language) throws ApiException {
+			@Valid @ApiParam(value = "language") @RequestParam(value = "language", defaultValue = "EN") Language language) throws ApiException {
 
-		return new ApiResponse<>(processingActionService.getProcessingAction(id, Language.valueOf(language)));
+		return new ApiResponse<>(processingActionService.getProcessingAction(id, language));
 	}
 
 	@GetMapping("{id}/detail")
