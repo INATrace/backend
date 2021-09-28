98d15ed0c9b7c6307f2ba288d75c3aff0670fe7b
MusicDin <dm6067@student.uni-lj.si>
2021-09-28 22:57:12 +0200

    Fix missing ID in StockOrder response

--- src/main/java/com/abelium/inatrace/components/stockorder/mappers/StockOrderMapper.java
+++ src/main/java/com/abelium/inatrace/components/stockorder/mappers/StockOrderMapper.java
@@ -13,6 +13,7 @@ public class StockOrderMapper {
 
     public static ApiStockOrder toApiStockOrder(StockOrder entity) {
         ApiStockOrder apiStockOrder = new ApiStockOrder();
+        apiStockOrder.setId(entity.getId());
         apiStockOrder.setIdentifier(entity.getIdentifier());
         apiStockOrder.setUpdateTimestamp(entity.getUpdateTimestamp());
         apiStockOrder.setCreatorId(entity.getCreatorId());
