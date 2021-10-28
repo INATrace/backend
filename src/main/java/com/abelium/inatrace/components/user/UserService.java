f659c0662b9cea5b1ac535ebfb8a69863f24cf2e
Nejc Rebernik <nejc.rebernik@sunesis.si>
2021-10-28 15:49:01 +0200

    do not allow unactivated users to log in

--- src/main/java/com/abelium/inatrace/components/user/UserService.java
+++ src/main/java/com/abelium/inatrace/components/user/UserService.java
@@ -96,7 +96,7 @@ public class UserService extends BaseService {
 		if (user == null) {
 			throw new ApiException(ApiStatus.AUTH_ERROR, "Invalid credentials");
 		}
-		if (user.getStatus() == UserStatus.DEACTIVATED || user.getStatus() == UserStatus.UNCONFIRMED) {
+		if (user.getStatus() == UserStatus.DEACTIVATED || user.getStatus() == UserStatus.UNCONFIRMED || user.getStatus() == UserStatus.CONFIRMED_EMAIL) {
 			throw new ApiException(ApiStatus.UNAUTHORIZED, "Not confirmed or disabled");
 		}
 		if (!new BCryptPasswordEncoder().matches(loginRequest.password, user.getPassword())) {
