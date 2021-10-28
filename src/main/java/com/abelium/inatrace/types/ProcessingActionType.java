ddbbb1d5d7e21e47b281a465749129017297dd11
Pece Adjievski <pece.adzievski@sunesis.si>
2021-10-28 10:27:51 +0200

    added processing action type for final product processing

--- src/main/java/com/abelium/inatrace/types/ProcessingActionType.java
+++ src/main/java/com/abelium/inatrace/types/ProcessingActionType.java
@@ -1,7 +1,22 @@
 package com.abelium.inatrace.types;
 
+/**
+ * Enum that holds the possible types of ation that Processing action can execute.
+ *
+ * PROCESSING - Processing a semi-product. We always have an input semi-product and a processed output semi-product.
+ *
+ * FINAL_PROCESSING - Processing an input semi-product into a final product. For example rosted coffe beans into packages for end customers.
+ *
+ * SHIPMENT - Also know as Quote action - Represent an Quote order placed between companies in the value chain (Product stakeholders).
+ * Can be also used for transfer final products between facilities. In this case, there is a field in ProcessingAction entity
+ * which specifeis that action is for final product.
+ *
+ * TRANSFER - Transfer a semi-product from one facility into onther. This is inside one company. Can be also used for transfer
+ * final products between facilities. In this case, there is a field in ProcessingAction entity which specifeis that action is for final product.
+ */
 public enum ProcessingActionType {
     PROCESSING,
+    FINAL_PROCESSING,
     SHIPMENT,
     TRANSFER
 }
