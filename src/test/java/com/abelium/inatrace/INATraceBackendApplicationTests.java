c48f9325951904b3a3965d53ac92970515bb1efe
Pece Adjievski <pece.adzievski@sunesis.si>
2022-01-04 21:24:07 +0100

    added image build script

--- src/test/java/com/abelium/inatrace/INATraceBackendApplicationTests.java
+++ src/test/java/com/abelium/inatrace/INATraceBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.abelium.inatrace;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class INATraceBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
