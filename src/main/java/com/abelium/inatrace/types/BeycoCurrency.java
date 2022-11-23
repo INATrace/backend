af3a73425174d2cc1ab2fb123be0edd75e7e058a
terli00 <uros.terlikar@gmail.com>
2022-11-23 10:45:04 +0100

    Change Beyco enums to TitleCase (addition)

--- src/main/java/com/abelium/inatrace/types/BeycoCurrency.java
+++ src/main/java/com/abelium/inatrace/types/BeycoCurrency.java
@@ -2,19 +2,8 @@ package com.abelium.inatrace.types;
 
 public enum BeycoCurrency {
 
-    USD("Usd"),
-    USD_CT("UsdCt"),
-    EUR("Eur");
-
-    private final String currency;
-
-    BeycoCurrency(String s) {
-        this.currency = s;
-    }
-
-    @Override
-    public String toString() {
-        return this.currency;
-    }
+    Usd,
+    UsdCt,
+    Eur
 
 }
