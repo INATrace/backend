fd532d561b841f8ecc1dd4d5f0c99093f85d12d5
Pece Adjievski <pece.adzievski@sunesis.si>
2023-01-08 12:22:11 +0100

    added docs. for PermissionsUtil.java

--- src/main/java/com/abelium/inatrace/security/utils/PermissionsUtil.java
+++ src/main/java/com/abelium/inatrace/security/utils/PermissionsUtil.java
@@ -12,24 +12,38 @@ import com.abelium.inatrace.types.UserRole;
 
 import java.util.List;
 
+/**
+ * Utility class for checking if user has required permission and/or enrollments.
+ *
+ * @author Pece Adjievski, Sunesis d.o.o.
+ */
 public final class PermissionsUtil {
 
 	private PermissionsUtil() {
 		throw new IllegalStateException("Utility class");
 	}
 
+	/**
+	 * Checks if the requesting user is part of a particular company.
+	 */
 	public static void checkUserIfCompanyEnrolled(List<CompanyUser> companyUsers, CustomUserDetails userToCheck) throws ApiException {
 		if (companyUsers.stream().noneMatch(cu -> cu.getUser().getId().equals(userToCheck.getUserId()))) {
 			throw new ApiException(ApiStatus.UNAUTHORIZED, "Unknown user company!");
 		}
 	}
 
+	/**
+	 * Checks if the requesting user is part of a particular company or a System admin.
+	 */
 	public static void checkUserIfCompanyEnrolledOrSystemAdmin(List<CompanyUser> companyUsers, CustomUserDetails userToCheck) throws ApiException {
 		if (!UserRole.ADMIN.equals(userToCheck.getUserRole()) && companyUsers.stream().noneMatch(cu -> cu.getUser().getId().equals(userToCheck.getUserId()))) {
 			throw new ApiException(ApiStatus.UNAUTHORIZED, "Unknown user company or not system admin!");
 		}
 	}
 
+	/**
+	 * Checks if the requesting user is a Company admin or a System admin.
+	 */
 	public static void checkUserIfCompanyAdminOrSystemAdmin(List<CompanyUser> companyUsers, CustomUserDetails userToCheck) throws ApiException {
 
 		if (UserRole.ADMIN.equals(userToCheck.getUserRole())) {
@@ -42,6 +56,9 @@ public final class PermissionsUtil {
 		}
 	}
 
+	/**
+	 * Checks if the requesting user is part of a particular company and is Company or System admin.
+	 */
 	public static void checkUserIfCompanyEnrolledAndAdminOrSystemAdmin(List<CompanyUser> companyUsers, CustomUserDetails userToCheck) throws ApiException {
 
 		CompanyUser companyUser = findCompanyUser(userToCheck.getUserId(), companyUsers);
@@ -51,6 +68,9 @@ public final class PermissionsUtil {
 		}
 	}
 
+	/**
+	 * Checks if the requesting user is part of a at least one company that is connected with one or more products.
+	 */
 	public static void checkUserIfConnectedWithProducts(List<Product> products, CustomUserDetails userToCheck) throws ApiException {
 
 		boolean userIsAssociatedWithOneOfTheProducts = false;
