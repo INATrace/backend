cc4dc087bd5955aeebbe2e55860eb6ab850fbe22
Pece Adjievski <pece.adzievski@sunesis.si>
2023-05-24 17:44:03 +0200

    Added new option REGIONAL_ADMIN in UserRole enum

--- src/main/java/com/abelium/inatrace/types/UserRole.java
+++ src/main/java/com/abelium/inatrace/types/UserRole.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 public enum UserRole implements GrantedAuthority {
     
 	USER("USER"),
-    SYSTEM_ADMIN("SYSTEM_ADMIN");
+    SYSTEM_ADMIN("SYSTEM_ADMIN"),
+	REGIONAL_ADMIN("REGIONAL_ADMIN");
 	
 	private final String authority;
 
