fdee941704b9f9d0907e169d439c9a14111cf2e2
Borche Paspalovski <borce.paspalovski@cloud.si>
2023-06-16 15:50:51 +0200

    fixed month function

--- src/main/java/com/abelium/inatrace/components/dashboard/DashboardService.java
+++ src/main/java/com/abelium/inatrace/components/dashboard/DashboardService.java
@@ -958,29 +958,29 @@ public class DashboardService extends BaseService {
      */
     private String translateMonth(String monthNum) {
         switch (monthNum) {
-            case "0":
-                return "Jan";
             case "1":
-                return "Feb";
+                return "Jan";
             case "2":
-                return "Mar";
+                return "Feb";
             case "3":
-                return "Apr";
+                return "Mar";
             case "4":
-                return "May";
+                return "Apr";
             case "5":
-                return "Jun";
+                return "May";
             case "6":
-                return "Jul";
+                return "Jun";
             case "7":
-                return "Aug";
+                return "Jul";
             case "8":
-                return "Sep";
+                return "Aug";
             case "9":
-                return "Oct";
+                return "Sep";
             case "10":
-                return "Nov";
+                return "Oct";
             case "11":
+                return "Nov";
+            case "12":
                 return "Dec";
         }
         return null;
