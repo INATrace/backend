50bae7a326db36bf8a8366a87d7d14ee1bc6fd84
Pece Adjievski <pece.adzievski@sunesis.si>
2024-09-19 14:20:53 +0200

    WIP: Spring Boot version upgrade

--- src/main/java/com/abelium/inatrace/configuration/CustomMySQLDialect.java
+++ src/main/java/com/abelium/inatrace/configuration/CustomMySQLDialect.java
@@ -1,18 +1,18 @@
 package com.abelium.inatrace.configuration;
 
+import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.dialect.MySQLDialect;
 import org.hibernate.dialect.function.StandardSQLFunction;
-import org.hibernate.query.spi.QueryEngine;
 import org.hibernate.type.StandardBasicTypes;
 
 public class CustomMySQLDialect extends MySQLDialect {
 
     @Override
-    public void initializeFunctionRegistry(QueryEngine queryEngine) {
+    public void initializeFunctionRegistry(FunctionContributions functionContributions) {
 
-        super.initializeFunctionRegistry(queryEngine);
+        super.initializeFunctionRegistry(functionContributions);
 
-        queryEngine.getSqmFunctionRegistry()
+        functionContributions.getFunctionRegistry()
                 .register("GROUP_CONCAT", new StandardSQLFunction("group_concat", StandardBasicTypes.STRING));
     }
 
